c5b492fab089800d43bbe672d31c7f7614876e45
dotrung16 <dvt1607sla@gmail.com>
2026-06-28 23:07:57 +0700

    feat: add volume control, track removal, and encapsulate media player

--- include/audioplayer.h
+++ include/audioplayer.h
@@ -6,7 +6,7 @@
 #include <QUrl>
 #include <QMediaMetaData>
 #include <QImage>
-#include <vector>
+#include <QList>
 
 class AudioPlayer : public QObject
 {
@@ -16,26 +16,34 @@ public:
     explicit AudioPlayer(QObject *parent = nullptr);
 
     void addTrack(const QUrl &url);
+    void removeTrack(int index);
+    void clearPlaylist();
     void playTrack(int index);
     void togglePlayPause();
     void nextTrack();
     void previousTrack();
 
-    QMediaPlayer* getPlayer() const { return player; }
+public slots:
+    void setPosition(int position);
+    void setVolume(float volume);
 
 signals:
     void trackChanged(const QString &fileName);
+    void trackIndexChanged(int index);
     void playbackStateChanged(bool isPlaying);
     void coverArtChanged(const QImage &image);
+    void positionChanged(qint64 position);
+    void durationChanged(qint64 duration);
+    void errorOccurred(const QString &errorString);
 
 private slots:
     void handleMediaStatus(QMediaPlayer::MediaStatus status);
     void extractMetaData();
+    void handleError(QMediaPlayer::Error error, const QString &errorString);
 
 private:
     QMediaPlayer *player;
     QAudioOutput *audioOutput;
-
-    std::vector<QUrl> playlist;
+    QList<QUrl> playlist;
     int currentIndex;
 };
--- src/audioplayer.cpp
+++ src/audioplayer.cpp
@@ -11,13 +11,47 @@ AudioPlayer::AudioPlayer(QObject *parent) : QObject(parent), currentIndex(-1)
 
     connect(player, &QMediaPlayer::mediaStatusChanged, this, &AudioPlayer::handleMediaStatus);
     connect(player, &QMediaPlayer::metaDataChanged, this, &AudioPlayer::extractMetaData);
+    
+    connect(player, &QMediaPlayer::positionChanged, this, &AudioPlayer::positionChanged);
+    connect(player, &QMediaPlayer::durationChanged, this, &AudioPlayer::durationChanged);
+    connect(player, &QMediaPlayer::errorOccurred, this, &AudioPlayer::handleError);
 }
 
 void AudioPlayer::addTrack(const QUrl &url)
 {
-    playlist.push_back(url);
-
+    playlist.append(url);
     if (currentIndex == -1) currentIndex = 0;
+}
+
+void AudioPlayer::removeTrack(int index)
+{
+    if (index >= 0 && index < playlist.size()) 
+    {
+        playlist.removeAt(index);
+        
+        if (index < currentIndex) {
+            currentIndex--;
+        } else if (index == currentIndex) {
+            if (playlist.isEmpty()) {
+                player->stop();
+                currentIndex = -1;
+                emit trackChanged("No track loaded");
+                emit coverArtChanged(QImage());
+            } else {
+                currentIndex = currentIndex % playlist.size();
+                playTrack(currentIndex);
+            }
+        }
+    }
+}
+
+void AudioPlayer::clearPlaylist()
+{
+    playlist.clear();
+    player->stop();
+    currentIndex = -1;
+    emit trackChanged("No track loaded");
+    emit coverArtChanged(QImage());
 }
 
 void AudioPlayer::playTrack(int index)
@@ -32,6 +66,7 @@ void AudioPlayer::playTrack(int index)
         QFileInfo fileInfo(playlist[currentIndex].toLocalFile());
 
         emit trackChanged(fileInfo.fileName());
+        emit trackIndexChanged(currentIndex); 
         emit playbackStateChanged(true);
     }
 }
@@ -41,34 +76,39 @@ void AudioPlayer::togglePlayPause()
     if (player->playbackState() == QMediaPlayer::PlayingState)
     {
         player->pause();
-
         emit playbackStateChanged(false);
     }
-    else if (!playlist.empty())
+    else if (!playlist.isEmpty())
     {
         if (player->source().isEmpty()) playTrack(currentIndex);
         else player->play();
-
         emit playbackStateChanged(true);
     }
 }
 
 void AudioPlayer::nextTrack()
 {
-    if (playlist.empty()) return;
-
+    if (playlist.isEmpty()) return;
     int nextIndex = (currentIndex + 1) % playlist.size();
-
     playTrack(nextIndex);
 }
 
 void AudioPlayer::previousTrack()
 {
-    if (playlist.empty()) return;
+    if (playlist.isEmpty()) return;
 
-    int prevIndex = (currentIndex - 1 + playlist.size()) % playlist.size();
-
+    int prevIndex = (currentIndex - 1 + static_cast<int>(playlist.size())) % playlist.size();
     playTrack(prevIndex);
+}
+
+void AudioPlayer::setPosition(int position)
+{
+    player->setPosition(position);
+}
+
+void AudioPlayer::setVolume(float volume)
+{
+    audioOutput->setVolume(volume);
 }
 
 void AudioPlayer::handleMediaStatus(QMediaPlayer::MediaStatus status)
@@ -82,22 +122,21 @@ void AudioPlayer::handleMediaStatus(QMediaPlayer::MediaStatus status)
 void AudioPlayer::extractMetaData()
 {
     QMediaMetaData metadata = player->metaData();
-
     QVariant coverVariant = metadata.value(QMediaMetaData::CoverArtImage);
 
-    if (coverVariant.isNull())
-    {
+    if (coverVariant.isNull()) {
         coverVariant = metadata.value(QMediaMetaData::ThumbnailImage);
     }
 
-    if (coverVariant.canConvert<QImage>())
-    {
-        QImage image = coverVariant.value<QImage>();
-
-        emit coverArtChanged(image);
-    }
-    else
-    {
+    if (coverVariant.canConvert<QImage>()) {
+        emit coverArtChanged(coverVariant.value<QImage>());
+    } else {
         emit coverArtChanged(QImage());
     }
+}
+
+void AudioPlayer::handleError(QMediaPlayer::Error error, const QString &errorString)
+{
+    Q_UNUSED(error);
+    emit errorOccurred(errorString);
 }

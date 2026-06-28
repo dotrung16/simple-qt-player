410c834f4e2a7d20551c5ecac46f4d703cb35d3e
dotrung16 <dvt1607sla@gmail.com>
2026-06-28 23:08:18 +0700

    feat: implement UI controls, fix playlist sync, and correct flac typo

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -16,13 +16,19 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow() = default;
+
 private slots:
     void openFiles();
+    void removeSelectedTrack();
+    void clearPlaylist();
     void update_UI_State(bool isPlaying);
     void updatePosition(qint64 position);
     void updateDuration(qint64 duration);
-    void seekPosition(int position);
     void updateCoverArt(const QImage &image);
+    void formatTime(qint64 position, qint64 duration);
+    void handlePlaybackError(const QString &errorMsg);
+    void changeVolume(int value);
+
 private:
     AudioPlayer *audio_conf;
 
@@ -30,8 +36,14 @@ private:
     QPushButton *nextButton;
     QPushButton *prevButton;
     QPushButton *addButton;
+    QPushButton *removeButton;
+    QPushButton *clearButton;
     QSlider *progressSlider;
+    QSlider *volumeSlider;
     QListWidget *playlistWidget;
     QLabel *nowPlayingLabel;
     QLabel *coverLabel;
+    QLabel *timeLabel;
+    
+    qint64 currentDuration;
 };
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,9 +1,12 @@
 #include "mainwindow.h"
 #include <QVBoxLayout>
+#include <QHBoxLayout>
 #include <QFileDialog>
 #include <QWidget>
+#include <QTime>
+#include <QMessageBox>
 
-MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), currentDuration(0)
 {
     audio_conf = new AudioPlayer(this);
 
@@ -13,59 +16,102 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
     coverLabel = new QLabel("No Cover Art", this);
     coverLabel->setAlignment(Qt::AlignCenter);
     coverLabel->setFixedSize(250, 250);
-    coverLabel->setStyleSheet("QLabel { background-color: #222; color: white; border-radius: 10px; }");
+    coverLabel->setObjectName("coverLabel"); 
 
     nowPlayingLabel = new QLabel("No track loaded", this);
+    nowPlayingLabel->setAlignment(Qt::AlignCenter);
+
     playlistWidget = new QListWidget(this);
     progressSlider = new QSlider(Qt::Horizontal, this);
+    timeLabel = new QLabel("00:00 / 00:00", this);
+
+    QHBoxLayout *progressLayout = new QHBoxLayout();
+    progressLayout->addWidget(progressSlider);
+    progressLayout->addWidget(timeLabel);
 
     QHBoxLayout *controlsLayout = new QHBoxLayout();
-    addButton = new QPushButton("Add Files", this);
     prevButton = new QPushButton("|<", this);
     playButton = new QPushButton("Play", this);
     nextButton = new QPushButton(">|", this);
+    
+    volumeSlider = new QSlider(Qt::Horizontal, this);
+    volumeSlider->setRange(0, 100);
+    volumeSlider->setValue(50);
+    volumeSlider->setFixedWidth(100);
 
-    controlsLayout->addWidget(addButton);
     controlsLayout->addWidget(prevButton);
     controlsLayout->addWidget(playButton);
     controlsLayout->addWidget(nextButton);
+    controlsLayout->addStretch();
+    controlsLayout->addWidget(new QLabel("Vol:", this));
+    controlsLayout->addWidget(volumeSlider);
+
+    QHBoxLayout *playlistControlsLayout = new QHBoxLayout();
+    addButton = new QPushButton("Add Files", this);
+    removeButton = new QPushButton("Remove", this);
+    clearButton = new QPushButton("Clear All", this);
+    
+    playlistControlsLayout->addWidget(addButton);
+    playlistControlsLayout->addWidget(removeButton);
+    playlistControlsLayout->addWidget(clearButton);
 
     mainLayout->addWidget(coverLabel, 0, Qt::AlignCenter);
     mainLayout->addWidget(nowPlayingLabel);
-    mainLayout->addWidget(progressSlider);
+    mainLayout->addLayout(progressLayout);
     mainLayout->addLayout(controlsLayout);
+    mainLayout->addLayout(playlistControlsLayout);
     mainLayout->addWidget(playlistWidget);
 
     setCentralWidget(centralWidget);
-    resize(400, 500);
 
     connect(addButton, &QPushButton::clicked, this, &MainWindow::openFiles);
+    connect(removeButton, &QPushButton::clicked, this, &MainWindow::removeSelectedTrack);
+    connect(clearButton, &QPushButton::clicked, this, &MainWindow::clearPlaylist);
+    
     connect(playButton, &QPushButton::clicked, audio_conf, &AudioPlayer::togglePlayPause);
     connect(nextButton, &QPushButton::clicked, audio_conf, &AudioPlayer::nextTrack);
     connect(prevButton, &QPushButton::clicked, audio_conf, &AudioPlayer::previousTrack);
     connect(playlistWidget, &QListWidget::currentRowChanged, audio_conf, &AudioPlayer::playTrack);
+    
+    connect(volumeSlider, &QSlider::valueChanged, this, &MainWindow::changeVolume);
+    connect(progressSlider, &QSlider::sliderMoved, audio_conf, &AudioPlayer::setPosition);
 
     connect(audio_conf, &AudioPlayer::playbackStateChanged, this, &MainWindow::update_UI_State);
     connect(audio_conf, &AudioPlayer::trackChanged, nowPlayingLabel, &QLabel::setText);
     connect(audio_conf, &AudioPlayer::coverArtChanged, this, &MainWindow::updateCoverArt);
-
-    connect(audio_conf->getPlayer(), &QMediaPlayer::positionChanged, this, &MainWindow::updatePosition);
-    connect(audio_conf->getPlayer(), &QMediaPlayer::durationChanged, this, &MainWindow::updateDuration);
-    connect(progressSlider, &QSlider::sliderMoved, this, &MainWindow::seekPosition);
+    connect(audio_conf, &AudioPlayer::positionChanged, this, &MainWindow::updatePosition);
+    connect(audio_conf, &AudioPlayer::durationChanged, this, &MainWindow::updateDuration);
+    connect(audio_conf, &AudioPlayer::trackIndexChanged, playlistWidget, [this](int row) {
+        playlistWidget->setCurrentRow(row);
+    });
+    connect(audio_conf, &AudioPlayer::errorOccurred, this, &MainWindow::handlePlaybackError);
 }
 
 void MainWindow::openFiles()
 {
-    QStringList files = QFileDialog::getOpenFileNames(this, "Open Audio File", "", "Audio File (*.mp3, *.wav, *.ogg, *flac)");
+    QStringList files = QFileDialog::getOpenFileNames(this, "Open Audio File", "", "Audio File (*.mp3, *.wav, *.ogg, *.flac)");
 
     for (const QString &file : files)
     {
         audio_conf->addTrack(QUrl::fromLocalFile(file));
-
         QFileInfo fileInfo(file);
-
         playlistWidget->addItem(fileInfo.fileName());
     }
+}
+
+void MainWindow::removeSelectedTrack()
+{
+    int row = playlistWidget->currentRow();
+    if (row >= 0) {
+        delete playlistWidget->takeItem(row);
+        audio_conf->removeTrack(row);
+    }
+}
+
+void MainWindow::clearPlaylist()
+{
+    playlistWidget->clear();
+    audio_conf->clearPlaylist();
 }
 
 void MainWindow::update_UI_State(bool isPlaying)
@@ -75,34 +121,49 @@ void MainWindow::update_UI_State(bool isPlaying)
 
 void MainWindow::updatePosition(qint64 position)
 {
-    if (!progressSlider->isSliderDown())
-    {
+    if (!progressSlider->isSliderDown()) {
         progressSlider->setValue(position);
     }
+    formatTime(position, currentDuration);
 }
 
 void MainWindow::updateDuration(qint64 duration)
 {
+    currentDuration = duration;
     progressSlider->setRange(0, duration);
+    formatTime(progressSlider->value(), duration);
 }
 
-void MainWindow::seekPosition(int position)
+void MainWindow::formatTime(qint64 position, qint64 duration)
 {
-    audio_conf->getPlayer()->setPosition(position);
+    QTime currentTime(0, 0, 0);
+    currentTime = currentTime.addMSecs(position);
+    
+    QTime totalTime(0, 0, 0);
+    totalTime = totalTime.addMSecs(duration);
+
+    QString format = duration > 3600000 ? "hh:mm:ss" : "mm:ss";
+    timeLabel->setText(currentTime.toString(format) + " / " + totalTime.toString(format));
 }
 
 void MainWindow::updateCoverArt(const QImage &image)
 {
-    if (image.isNull())
-    {
+    if (image.isNull()) {
         coverLabel->setPixmap(QPixmap());
         coverLabel->setText("No Cover Art");
-    }
-    else
-    {
+    } else {
         QPixmap pixmap = QPixmap::fromImage(image);
         QPixmap scaledPixmap = pixmap.scaled(coverLabel->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation);
-
         coverLabel->setPixmap(scaledPixmap);
     }
+}
+
+void MainWindow::handlePlaybackError(const QString &errorMsg)
+{
+    QMessageBox::warning(this, "Playback Error", "Error playing media: " + errorMsg);
+}
+
+void MainWindow::changeVolume(int value)
+{
+    audio_conf->setVolume(value / 100.0f);
 }
